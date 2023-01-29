704a7d363c30df8ed26571c00bba1bdbcbbfba9c
Josip Kilić <neksuses@gmail.com>
2023-01-29 19:09:47 +0100

    Flutter 3.7.0 updates

--- lib/widgets/hero_title.dart
+++ lib/widgets/hero_title.dart
@@ -1,4 +1,3 @@
-import 'package:dough/dough.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -16,65 +15,63 @@ class HeroTitle extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => PressableDough(
-        child: Padding(
-          padding: EdgeInsets.only(top: 16.h),
-          child: AnimatedColumn(
-            children: [
-              Container(
-                alignment: Alignment.topLeft,
-                padding: EdgeInsets.only(left: 32.w),
-                child: SvgPicture.asset(
-                  ModerniAliasImages.conversationUpImage,
-                  width: 70.w,
-                ),
+  Widget build(BuildContext context) => Padding(
+        padding: EdgeInsets.only(top: 16.h),
+        child: AnimatedColumn(
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.only(left: 32.w),
+              child: SvgPicture.asset(
+                ModerniAliasImages.conversationUpImage,
+                width: 70.w,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: 12.h,
-                      bottom: smallText == null ? 12.h : 0,
-                    ),
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        text: 'appNameFirstString'.tr,
-                        style: ModerniAliasTextStyles.appNameFirst,
-                        children: [
-                          TextSpan(
-                            text: 'appNameSecondString'.tr,
-                            style: ModerniAliasTextStyles.appNameSecond,
-                          ),
-                        ],
-                      ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 12.h,
+                    bottom: smallText == null ? 12.h : 0,
+                  ),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: 'appNameFirstString'.tr,
+                      style: ModerniAliasTextStyles.appNameFirst,
+                      children: [
+                        TextSpan(
+                          text: 'appNameSecondString'.tr,
+                          style: ModerniAliasTextStyles.appNameSecond,
+                        ),
+                      ],
                     ),
                   ),
-                  if (smallText != null)
-                    Padding(
-                      padding: EdgeInsets.only(
-                        bottom: 16.h,
-                        right: 8.w,
-                      ),
-                      child: Text(
-                        smallText!,
-                        style: ModerniAliasTextStyles.smallHero,
-                        textAlign: TextAlign.end,
-                      ),
-                    ),
-                ],
-              ),
-              Container(
-                alignment: Alignment.bottomRight,
-                padding: EdgeInsets.only(right: 32.w),
-                child: SvgPicture.asset(
-                  ModerniAliasImages.conversationDownImage,
-                  width: 70.w,
                 ),
+                if (smallText != null)
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: 16.h,
+                      right: 8.w,
+                    ),
+                    child: Text(
+                      smallText!,
+                      style: ModerniAliasTextStyles.smallHero,
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+              ],
+            ),
+            Container(
+              alignment: Alignment.bottomRight,
+              padding: EdgeInsets.only(right: 32.w),
+              child: SvgPicture.asset(
+                ModerniAliasImages.conversationDownImage,
+                width: 70.w,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
 }
