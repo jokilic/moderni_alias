9510afbf2ebcb5ab1895d44f5f9c28c914686d92
Josip Kilić <neksuses@gmail.com>
2025-01-17 04:24:56 +0100

    Put explicit color on every `TextStyle`

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -9,16 +9,19 @@ class ModerniAliasTextStyles {
     fontWeight: FontWeight.w700,
   );
   static const holdMeExitButton = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 14,
     fontWeight: FontWeight.w700,
   );
   static const exitModal = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const flagName = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
@@ -30,224 +33,260 @@ class ModerniAliasTextStyles {
     fontWeight: FontWeight.w400,
   );
   static const gameOffStart = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const gameCurrentWord = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 50,
     fontWeight: FontWeight.w700,
     letterSpacing: 8,
   );
   static const gameCurrentSecond = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 120,
     fontWeight: FontWeight.w700,
     letterSpacing: 8,
   );
   static const gameTitle = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 34,
     fontWeight: FontWeight.w700,
   );
   static const gameTitleSmall = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 26,
     fontWeight: FontWeight.w700,
   );
   static const appNameFirst = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 60,
     fontWeight: FontWeight.w400,
   );
   static const appNameSecond = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 40,
     fontWeight: FontWeight.w700,
   );
   static const smallHero = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 14,
     fontWeight: FontWeight.w700,
   );
   static const playButton = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 18,
     fontWeight: FontWeight.w700,
   );
   static const smallTitle = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 24,
     fontWeight: FontWeight.w700,
   );
   static const winnerFirst = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const winnerTeam = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 34,
     fontWeight: FontWeight.w700,
   );
   static const winnerPoints = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 34,
     fontWeight: FontWeight.w700,
   );
   static const generalInfo = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w400,
     height: 1.45,
   );
   static const generalInfoBold = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     height: 1.45,
   );
   static const generalInfoBoldBlue = TextStyle(
-    fontFamily: 'Sen',
     color: ModerniAliasColors.blue,
+    fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     height: 1.45,
   );
   static const stats = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     height: 1.4,
   );
   static const statsNumber = TextStyle(
-    fontFamily: 'Sen',
     color: ModerniAliasColors.darkBlue,
+    fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     height: 1.4,
   );
   static const howToPlay = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w400,
     height: 1.45,
   );
   static const howToPlayBold = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     height: 1.45,
   );
   static const howToPlayBoldGreen = TextStyle(
-    fontFamily: 'Sen',
     color: ModerniAliasColors.green,
+    fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     height: 1.45,
   );
   static const howToPlayBoldRed = TextStyle(
-    fontFamily: 'Sen',
     color: ModerniAliasColors.red,
+    fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     height: 1.45,
   );
   static const highscore = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const playingTeamTitle = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 14,
     fontWeight: FontWeight.w400,
   );
   static const playingTeam = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 34,
     fontWeight: FontWeight.w700,
   );
   static const scoresTitle = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 34,
     fontWeight: FontWeight.w700,
   );
   static const quickWrongScore = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 34,
     fontWeight: FontWeight.w700,
   );
   static const quickCorrectScore = TextStyle(
-    fontFamily: 'Sen',
     color: ModerniAliasColors.blue,
+    fontFamily: 'Sen',
     fontSize: 34,
     fontWeight: FontWeight.w700,
   );
   static const quickGameFinished = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const quickGameFinishedBold = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 34,
     fontWeight: FontWeight.w700,
   );
   static const gameButton = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 30,
     fontWeight: FontWeight.w700,
   );
   static const nameOfTeamHint = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const validation = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const howToPlayButton = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 13,
     fontWeight: FontWeight.w600,
   );
   static const teamNameTextField = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const statsSegmentedControl = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 22,
     fontWeight: FontWeight.w700,
   );
   static const timeGameClock = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 32,
     fontWeight: FontWeight.w700,
   );
   static const timeGameNumberOfWords = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 20,
     fontWeight: FontWeight.w700,
   );
   static const settingsBigTitle = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 32,
     fontWeight: FontWeight.w700,
     height: 1.4,
   );
   static const settingsTitle = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 24,
     fontWeight: FontWeight.w700,
     height: 1.4,
   );
   static const settingsSubtitle = TextStyle(
+    color: ModerniAliasColors.white,
     fontFamily: 'Sen',
     fontSize: 15,
     fontWeight: FontWeight.w700,
