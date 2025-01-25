253dff0c83f3d30949bc466c1ce7b7257572799b
Josip Kilić <neksuses@gmail.com>
2025-01-25 23:14:14 +0100

    Update background color in icon & splash

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/painting.dart';
 
 class ModerniAliasColors {
   static const darkBlue = Color(0xFF152544);
+  static const lightBlue = Color(0xFF1C3D5F);
   static const black = Color(0xFF000000);
   static const white = Color(0xFFF1F1F1);
   static const red = Color(0xFFB05654);
