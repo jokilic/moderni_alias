48c169aa03087fca086790b677efb8d9750f49f5
Josip Kilić <neksuses@gmail.com>
2026-07-07 11:26:52 +0200

    Faster animations

--- lib/constants/durations.dart
+++ lib/constants/durations.dart
@@ -1,8 +1,7 @@
-// TODO: Bit faster
 class ModerniAliasDurations {
-  static const animation = Duration(milliseconds: 300);
-  static const slowAnimation = Duration(milliseconds: 600);
-  static const verySlowAnimation = Duration(milliseconds: 1200);
-  static const fastAnimation = Duration(milliseconds: 150);
-  static const veryFastAnimation = Duration(milliseconds: 75);
+  static const animation = Duration(milliseconds: 200);
+  static const slowAnimation = Duration(milliseconds: 400);
+  static const verySlowAnimation = Duration(milliseconds: 800);
+  static const fastAnimation = Duration(milliseconds: 100);
+  static const veryFastAnimation = Duration(milliseconds: 50);
 }
